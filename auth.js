d704ada5950e8e3fbe82a3d91c62d186b20ba291
Ejemy <eric.iwan@mail.com>
2023-02-11 19:43:39 +0900

    Just no encryption yet

--- auth.js
+++ auth.js
@@ -1,58 +1,85 @@
 require('dotenv').config()
-
-
-
-const passport = require("passport");
 const session = require("express-session");
 const localStrategy = require("passport-local");
-const mysql = require("mysql");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
 
 
 
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "PB23Group",
-    database: "ticketsystem"
-  })
-  
-  
 
 
   
 
-module.exports = function(app) { 
+module.exports = function(app, passport) { 
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.json());
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "PB23Group",
+        database: "IT_Ticketing_System"
+    })
+
+    
+
     console.log("Auth.js kicking in!")
     app.use(session({
         secret: process.env.SESSION_SECRET,
         resave: true,
         saveUninitialized: true,
         cookie: {secure: false}
-    }))
+      }))
     app.use(passport.initialize());
     app.use(passport.session());
 
 
-    con.connect(function(err){
-        if(err) console.log(err);
-        console.log("DB Connected...");
-        var sql = "INSERT INTO Users (FirstName) VALUES ('Eric')";
-        con.query(sql, function(err, result){
-            if(err) console.log(err);
-            console.log(result);
-        })
-      });
+    
 
-    passport.use(new localStrategy((user,pass,done)=>{
+    con.connect((err)=>{
+        console.log("DB connection outside of passport established...")
+      
+      
+      passport.use(new localStrategy({usernameField: 'email',
+      passwordField: 'password'}, function(email,password,done){
+        console.log("localstrategy user is...");
+        console.log(email);
+            var sql = "SELECT * FROM users WHERE Email = ?";
+            con.query(sql, [email],function(err, account){
+                
+                if(err) {
+                     return console.log(err);
+                }
+                if(!account){
+                  console.log("No account")
+                  return done(null, null);
+                }
+                con.query("SELECT Password FROM users WHERE Email = ?", [email], function(err, pass){
+                    if(password !== pass.Password){
+                      return done(null,account);  
+                    }
+                    
+                }  )
+                
+            })
         
-    }))
-
-    passport.serializeUser((user,done)=>{
+      }))
+      
+      passport.serializeUser((user,done)=>{
+        console.log("Serialize...")
         done(null, user);
-    })
-    passport.deserializeUser((id,done)=>{
+      })
+      passport.deserializeUser((user, done)=>{
         //search DB for user by id and return that table.
-    })
+        console.log("Deserialize...");
+            con.query("SELECT * FROM Users WHERE UserID = ?",[user[0].UserID], function(err, result){
+                if(err) throw err;
+                return done(null,result);
+            })
+        
+      })
+      })
 
 }
+
 
